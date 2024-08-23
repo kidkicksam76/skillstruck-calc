ee13030427fa29121cd64a5908ba6b4b9d917757
take the L <118221675+kidkicksam76@users.noreply.github.com>
2024-08-23 08:13:00 -0700

    exported from Skill Struck

---
+++ calculators /quadratics calculator (add than sub) .py
@@ -0,0 +1,17 @@
+print("you are adding than subtracting")
+
+number_1 = int(input("Enter first number with the bigger x:  "))
+number_3 = int(input("enter second number with the smaller x: "))
+
+answer1 = (number_1 + number_3)
+print("answer for the two x's ", answer1)
+
+number_2 = int(input("Enter first number the bigger one: "))
+number_4 = int(input("enter second number the smaller: "))
+
+answer4 = (number_2 - number_4)
+print(answer4)
+final_answer3 = (answer1 / answer4)
+print(final_answer3)
+
+quit()
