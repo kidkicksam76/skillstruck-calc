10c2116df86c209efa0d12a9816e3bf3b4945b72
take the L <118221675+kidkicksam76@users.noreply.github.com>
2024-08-23 08:12:59 -0700

    exported from Skill Struck

---
+++ calculators /quadratics calculator (sub) .py
@@ -0,0 +1,17 @@
+print("subtraction")
+
+
+number_1 = int(input("Enter first number with the bigger x:  "))
+number_3 = int(input("enter second number with the smaller x: "))
+
+answer3 = (number_1 - number_3)
+print("answer for the two x's ", answer3)
+
+number_2 = int(input("Enter first number the bigger one: "))
+number_4 = int(input("enter second number the smaller: "))
+
+
+answer4 = (number_2 - number_4)
+print(answer4)
+final_answer3 = answer3 / answer4
+print(final_answer3)
