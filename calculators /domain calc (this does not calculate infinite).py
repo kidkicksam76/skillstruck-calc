e2b1722e0ce7370074e2ff2802d5f7d177d7b5c3
take the L <118221675+kidkicksam76@users.noreply.github.com>
2024-08-23 08:13:00 -0700

    exported from Skill Struck

---
+++ calculators /domain calc (this does not calculate infinite).py
@@ -0,0 +1,8 @@
+print("this does not account for infinity")
+
+left_most = int(input("what is your left most point"))
+right_most = int(input("what is your right most point"))
+
+print("this does not account for infinity")
+
+print((left_most ,right_most))
