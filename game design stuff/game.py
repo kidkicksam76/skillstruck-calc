071aead9e2d092ab527a5ee0292884a601cc97a9
take the L <118221675+kidkicksam76@users.noreply.github.com>
2024-08-23 08:12:57 -0700

    exported from Skill Struck

---
+++ game design stuff/game.py
@@ -0,0 +1,14 @@
+words_that_have_to_do_with_music = {
+    "word1": "music",
+    "word2": "punk rock",
+    "word3": "pop",
+    "word4": "metal",
+    "word5": "rock",
+    "word6": "punk",
+    "word7": "death metal",
+    "word8": "alternitive",
+    "word9": "alternitive rock",
+    "word10": "guitar",
+}
+
+print(words_that_have_to_do_with_music)
