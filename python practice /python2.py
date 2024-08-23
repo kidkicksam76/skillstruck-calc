b2f7c448641f9d7de3d7af341a3a883b6ede8765
take the L <118221675+kidkicksam76@users.noreply.github.com>
2024-08-23 08:12:57 -0700

    exported from Skill Struck

---
+++ python practice /python2.py
@@ -0,0 +1,12 @@
+mountains = {
+    "Timpanogos" : 12,
+    "Everest" : 14,
+    "Kilimanjaro" : 16,
+    "Vesuvius" : 18
+}
+
+print(mountains)
+
+print(mountains["Vesuvius"])
+
+mountains["Kilimanjaro"] = 30
