7ec144e1e252a7565025d67be130c44a11a3e559
take the L <118221675+kidkicksam76@users.noreply.github.com>
2024-08-29 09:58:46 -0700

    exported from Skill Struck

--- python practice /intro.py
+++ python practice /intro.py
@@ -1,5 +1,9 @@
+I = ("I ")
+
+people = ("was involved with this murder ")
+
 classmates = {
-    "billy" : 8,
+    "Billy" : 8,
     "vance" : 12,
     "alice" : 10,
     "eliza" : 15,
@@ -7,4 +11,7 @@ classmates = {
 }
 
 for x in classmates:
-    if x == "eliza"
+    if x == "eliza":
+        print("this pirson wants to be anonymous")
+    else:
+        print(I + x + people)
